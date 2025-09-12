b8a6bc1f44923c1c1eb162e1c137ce8cdd85cdbb
George Boone <yongboone1@gmail.com>
2025-09-12 18:41:40 -0500

    someone messed with a file they werent supposed to...
    dont do that

--- VT_MSPM0G3507_A/src/Tasks/fiddle.cpp
+++ VT_MSPM0G3507_A/src/Tasks/fiddle.cpp
@@ -17,107 +17,106 @@
 #include <stdio.h>
 
 #include "Core/system.hpp"
-#include "Middleware/BQ76952.hpp"
 
-//SPI
-//void Task::fiddle_task(void *) {
-//    // - SPI0 goes up to 32Mhz. speed restraints depending on MCU speed. check DOCs.
-//
-//    DL_SPI_enablePower(System::spi0.reg);
-//    delay_cycles(POWER_STARTUP_DELAY);
-//
-//    /*--- GPIO config ----------------*/
-//
-//    DL_GPIO_initPeripheralOutputFunction(IOMUX_PINCM43, IOMUX_PINCM43_PF_SPI0_PICO);// MOSI , PB17
-//    DL_GPIO_initPeripheralInputFunctionFeatures(//    MISO , PB19
-//            IOMUX_PINCM45,
-//            IOMUX_PINCM45_PF_SPI0_POCI,
-//            DL_GPIO_INVERSION::DL_GPIO_INVERSION_DISABLE,
-//            DL_GPIO_RESISTOR::DL_GPIO_RESISTOR_NONE,
-//            DL_GPIO_HYSTERESIS::DL_GPIO_HYSTERESIS_DISABLE,
-//            DL_GPIO_WAKEUP::DL_GPIO_WAKEUP_DISABLE
-//        );
-//    DL_GPIO_enableHiZ(IOMUX_PINCM45);
-//    DL_GPIO_initPeripheralOutputFunctionFeatures(//    SCLK , PB18
-//            IOMUX_PINCM44,
-//            IOMUX_PINCM44_PF_SPI0_SCLK,
-//            DL_GPIO_INVERSION::DL_GPIO_INVERSION_DISABLE,
-//            DL_GPIO_RESISTOR::DL_GPIO_RESISTOR_NONE,
-//            DL_GPIO_DRIVE_STRENGTH::DL_GPIO_DRIVE_STRENGTH_HIGH,
-//            DL_GPIO_HIZ::DL_GPIO_HIZ_DISABLE
-//        );
-//    DL_GPIO_initDigitalOutputFeatures(//    CS2  , PB20
-//            IOMUX_PINCM48,
-//            DL_GPIO_INVERSION::DL_GPIO_INVERSION_ENABLE,
-//            DL_GPIO_RESISTOR::DL_GPIO_RESISTOR_NONE,
-//            DL_GPIO_DRIVE_STRENGTH::DL_GPIO_DRIVE_STRENGTH_HIGH,
-//            DL_GPIO_HIZ::DL_GPIO_HIZ_DISABLE
-//        );
-//    DL_SPI_setChipSelect(System::spi0.reg, DL_SPI_CHIP_SELECT::DL_SPI_CHIP_SELECT_NONE);
-//    DL_GPIO_enableOutput(GPIOB, DL_GPIO_PIN_17 | DL_GPIO_PIN_18 | DL_GPIO_PIN_19 | DL_GPIO_PIN_20);
-//
-//    System::GPIO::GPIO cs = {.port = GPIOB, .pin = DL_GPIO_PIN_20};
-//    cs.clear();
-//
-//    vTaskDelay(pdMS_TO_TICKS(100));
-//
-//    /*--- SPI config -----------------*/
-//
-//    System::spi0.partialInit();
-//    DL_SPI_Config config = {
-//            .mode           = DL_SPI_MODE::DL_SPI_MODE_CONTROLLER,
-//            .frameFormat    = DL_SPI_FRAME_FORMAT::DL_SPI_FRAME_FORMAT_MOTO4_POL0_PHA0,
-//            .parity         = DL_SPI_PARITY::DL_SPI_PARITY_NONE,
-//            .dataSize       = DL_SPI_DATA_SIZE::DL_SPI_DATA_SIZE_8,
-//            .bitOrder       = DL_SPI_BIT_ORDER::DL_SPI_BIT_ORDER_LSB_FIRST,
-//            .chipSelectPin  = DL_SPI_CHIP_SELECT::DL_SPI_CHIP_SELECT_2,
-//        };
-//    DL_SPI_init(System::spi0.reg, &config);
-//    DL_SPI_disablePacking(System::spi0.reg);
-//    System::spi0.setSCLKTarget(125e3);
-//    DL_SPI_enable(System::spi0.reg);
-//
-//    /*--- stuff ----------------------*/
-//
-//    uint8_t buff[10] = {0x22, BV(4)};
-//
-//    cs.set();
-//    System::spi0.tx_blocking(buff, 3, &cs);
-//    delay_cycles(80 * 100);
-//    System::spi0.rx_blocking(buff, 3, &cs);
-//
-//    System::uart_ui.nputs(ARRANDN("end of fiddle" NEWLINE));
-//
-//    vTaskDelete(NULL);
-//}
 
-// ti battery guage framework
-#include <ti/battery_gauge/gauge_level2/Gauge_Type.h>
-#include "ti_msp_dl_config.h"
 void Task::fiddle_task(void *){
     System::uart_ui.nputs(ARRANDN("fiddle task start" NEWLINE));
 
-    SYSCFG_DL_MCAN0_init();
+    DL_MCAN_reset(CANFD0);
+    DL_MCAN_enablePower(CANFD0);
+    delay_cycles(POWER_STARTUP_DELAY);
 
-    DL_MCAN_TxBufElement txMsg = {0};
+    constexpr DL_MCAN_ClockConfig clkConfig = {
+       .clockSel= DL_MCAN_FCLK::DL_MCAN_FCLK_SYSPLLCLK1,    // assuming is 20MHz
+       .divider = DL_MCAN_FCLK_DIV::DL_MCAN_FCLK_DIV_1,     // need to reach 20MHz
+    };
 
-    txMsg.id = (0x123 << 18);
-    txMsg.rtr = 0;
-    txMsg.xtd = 0;
-    txMsg.dlc = 0;
-    txMsg.data[0] = 0x11;
-    txMsg.data[1] = 0x22;
-    txMsg.data[2] = 0x33;
-    txMsg.data[3] = 0x44;
-    txMsg.data[4] = 0x55;
-    txMsg.data[5] = 0x66;
-    txMsg.data[6] = 0x77;
-    txMsg.data[7] = 0x88;
+    constexpr DL_MCAN_InitParams initparam = {
+      /* Initialize MCAN Init parameters.    */
+      .fdMode            = true,    // CAN FD mode?
+      .brsEnable         = true,    // enable bit rate switching?
+      .txpEnable         = true,    // pause for 2 bit times after successful transmission?
+      .efbi              = false,   // edge filtering? 2 consecutive Tq to accept sync
+      .pxhddisable       = false,   // do not Tx error frame on protocol error?
+      .darEnable         = false,   // auto retransmission of failed frames?
+      .wkupReqEnable     = true,    // enable wake up request?
+      .autoWkupEnable    = true,    // enable auto-wakeup?
+      .emulationEnable   = true,
+      .wdcPreload        = 255,     // start value of message ram WDT
 
-    DL_MCAN_writeMsgRam(MCAN0_INST, DL_MCAN_MEM_TYPE_BUF, 0U, &txMsg);
-    DL_MCAN_TXBufAddReq(MCAN0_INST, 0U);
+      /* Transmitter Delay Compensation parameters. tm.26.4.6/1439 */
+      .tdcEnable         = false,   // transmitter delay compensation
+      .tdcConfig.tdcf    = 10,      // filter length
+      .tdcConfig.tdco    = 6,       // filter offset
+    };
 
-    System::uart_ui.nputs(ARRANDN("CAN frame queued" NEWLINE));
+    constexpr DL_MCAN_MsgRAMConfigParams  ramConfig = {
+        /* Standard ID Filter List Start Address. */
+        .flssa                = 0,
+        /* List Size: Standard ID. */
+        .lss                  = 0,
+        /* Extended ID Filter List Start Address. */
+        .flesa                = 0,
+        /* List Size: Extended ID. */
+        .lse                  = 0,
+        /* Tx Buffers Start Address. */
+        .txStartAddr          = 12,
+        /* Number of Dedicated Transmit Buffers. */
+        .txBufNum             = 2,
+        .txFIFOSize           = 10,
+        /* Tx Buffer Element Size. */
+        .txBufMode            = 0,
+        .txBufElemSize        = DL_MCAN_ELEM_SIZE_64BYTES,
+        /* Tx Event FIFO Start Address. */
+        .txEventFIFOStartAddr = 640,
+        /* Event FIFO Size. */
+        .txEventFIFOSize      = 2,
+        /* Level for Tx Event FIFO watermark interrupt. */
+        .txEventFIFOWaterMark = 0,
+        /* Rx FIFO0 Start Address. */
+        .rxFIFO0startAddr     = 0,
+        /* Number of Rx FIFO elements. */
+        .rxFIFO0size          = 0,
+        /* Rx FIFO0 Watermark. */
+        .rxFIFO0waterMark     = 0,
+        .rxFIFO0OpMode        = 0,
+        /* Rx FIFO1 Start Address. */
+        .rxFIFO1startAddr     = 0,
+        /* Number of Rx FIFO elements. */
+        .rxFIFO1size          = 0,
+        /* Level for Rx FIFO 1 watermark interrupt. */
+        .rxFIFO1waterMark     = 0,
+        /* FIFO blocking mode. */
+        .rxFIFO1OpMode        = 0,
+        /* Rx Buffer Start Address. */
+        .rxBufStartAddr       = 0,
+        /* Rx Buffer Element Size. */
+        .rxBufElemSize        = DL_MCAN_ELEM_SIZE_64BYTES,
+        /* Rx FIFO0 Element Size. */
+        .rxFIFO0ElemSize      = DL_MCAN_ELEM_SIZE_64BYTES,
+        /* Rx FIFO1 Element Size. */
+        .rxFIFO1ElemSize      = DL_MCAN_ELEM_SIZE_64BYTES,
+    };
+
+    static const DL_MCAN_BitTimingParams   bitTimeingParams = {
+        /* Arbitration Baud Rate Pre-scaler. */
+        .nomRatePrescalar   = 40-1, // 0-> /1 , CAN clk divider to bit time
+        /* Arbitration Time segment before sample point. */
+        .nomTimeSeg1        = 138,
+        /* Arbitration Time segment after sample point. */
+        .nomTimeSeg2        = 19,
+        /* Arbitration (Re)Synchronization Jump Width Range. */
+        .nomSynchJumpWidth  = 19,
+        /* Data Baud Rate Pre-scaler. */
+        .dataRatePrescalar  = 0,
+        /* Data Time segment before sample point. */
+        .dataTimeSeg1       = 16,
+        /* Data Time segment after sample point. */
+        .dataTimeSeg2       = 1,
+        /* Data (Re)Synchronization Jump Width.   */
+        .dataSynchJumpWidth = 1,
+    };
+
     System::uart_ui.nputs(ARRANDN("fiddle task end" NEWLINE));
     vTaskDelete(NULL);
 }
