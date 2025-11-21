9043bf0b6ade2632b3a427bbefe2b3e8d1ffe8c7
Khai Nguyen <122934900+Khai-Nguyen-UTA@users.noreply.github.com>
2025-11-20 21:02:27 -0600

    sending!

---
+++ VT_MSPM0G3507_A/src/Core/BMS/BMSMaster.cpp
@@ -0,0 +1,9 @@
+/*
+ * BMSMaster.cpp
+ *
+ *  Created on: Nov 20, 2025
+ *      Author: turtl
+ */
+
+#include "BMSMaster.hpp"
+
--- VT_MSPM0G3507_A/src/Tasks/task_BMS.cpp
+++ VT_MSPM0G3507_A/src/Tasks/task_BMS.cpp
@@ -14,6 +14,8 @@
 #include "Core/FancyCli.hpp"
 #include "Middleware/BQ769x2/BQ76952.hpp"
 #include "Core/common.h"
+#include "Core/BMS/BMSComms.hpp"   // adjust path if needed
+
 
 // user amp to centiamp scale (10mA)
 // using 10mA scale (determined by DAConfiguration)
@@ -732,6 +734,47 @@ void Task::BMS_task(void *){
         vTaskDelay(pdMS_TO_TICKS(400));
         System::uart_ui.nputs(ARRANDN("meow" NEWLINE));
     };
+
+
+//    PacketHeader pktHeader;
+//    Pkt_CellV pktCellV;
+    BMSComms::PacketHeader Packetheader;
+    BMSComms::Pkt_CellV Pkt_cellV;
+
+
+    //pointer to variable -> point it to the start of the data array, no copy
+
+    while (1)
+    {
+        // Slave CAN TX
+        DL_MCAN_TxBufElement txmsg = {
+                .id     = 0x1,      // CAN id, 11b->[28:18], 29b->[] when using 11b can id
+                .rtr    = 0,        // 0: data frame, 1: remote frame
+                .xtd    = 1,        // 0: 11b id, 1: 29b id
+                .esi    = 0,        // error state indicator, 0: passive flag, 1: transmission recessive
+                .dlc    = 3,        // data byte count, see DL comments
+                .brs    = 0,        // 0: no bit rate switching, 1: yes brs
+                .fdf    = 1,        // FD format, 0: classic CAN, 1: CAN FD format
+                .efc    = 0,        // 0: dont store Tx events, 1: store
+                .mm     = 0x3,      // In order to track which transmit frame corresponds to which TX Event FIFO element, you can use the MM(Message Marker) bits in the transmit frame. The corresponding TX Event FIFO element will have the same message marker.
+            };
+
+        txmsg.data[0] = 6;
+        txmsg.data[1] = 7;
+        txmsg.data[2] = 8;
+
+        DL_MCAN_TxFIFOStatus tf;
+        DL_MCAN_getTxFIFOQueStatus(CANFD0, &tf);
+
+        uint32_t bufferIndex = tf.putIdx;
+
+        DL_MCAN_writeMsgRam(CANFD0, DL_MCAN_MEM_TYPE_FIFO, bufferIndex, &txmsg);
+        DL_MCAN_TXBufAddReq(CANFD0, tf.getIdx);
+
+        vTaskDelay(pdMS_TO_TICKS(400));
+        System::uart_ui.nputs(ARRANDN("meow" NEWLINE));
+    }
+
 
 
     System::FailHard("BMS_task ended" NEWLINE);
