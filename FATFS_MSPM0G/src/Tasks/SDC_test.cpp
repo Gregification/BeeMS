428631473159d3023e91d5d5094d75cd14c5d6d0
glezbryan <glezbryan12@gmail.com>
2026-03-30 12:02:52 -0500

    FILE ON SD CARD FOUND
     - we are writing 0 bytes

--- FATFS_MSPM0G/src/Tasks/SDC_test.cpp
+++ FATFS_MSPM0G/src/Tasks/SDC_test.cpp
@@ -14,6 +14,9 @@
 
 /*** setup ***************************************************/
 
+
+void printFS(FATFS fs);
+
 void Task::SDC_test_task(void *){
     System::UART::uart_ui.nputs(ARRANDN("SDC_test_task start" NEWLINE));
 
@@ -36,89 +39,102 @@ void Task::SDC_test_task(void *){
     System::UART::uart_ui.nputs(ARRANDN("\n\n\r"));
 
 
-    System::UART::uart_ui.nputs(ARRANDN("FAT sub type: "));
-    System::UART::uart_ui.putu32h(fs.fs_type);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("File status: "));
-    System::UART::uart_ui.putu32h(fs.flag);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("Number of sectors per cluster: "));
-    System::UART::uart_ui.putu32h(fs.csize);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("pad1: "));
-    System::UART::uart_ui.putu32h(fs.pad1);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("Number of root directory entries (0 on FAT32): "));
-    System::UART::uart_ui.putu32h(fs.n_rootdir);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("Number of FAT entries (= number of clusters + 2): "));
-    System::UART::uart_ui.putu32h(fs.n_fatent);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("FAT start sector: "));
-    System::UART::uart_ui.putu32h(fs.fatbase);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("Root directory start sector (Cluster# on FAT32): "));
-    System::UART::uart_ui.putu32h(fs.dirbase);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("Data start sector: "));
-    System::UART::uart_ui.putu32h(fs.database);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("File R/W pointer: "));
-    System::UART::uart_ui.putu32h(fs.fptr);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("File size: "));
-    System::UART::uart_ui.putu32h(fs.fsize);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("File start cluster: "));
-    System::UART::uart_ui.putu32h(fs.org_clust);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("File current cluster: "));
-    System::UART::uart_ui.putu32h(fs.curr_clust);
-    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
-
-    System::UART::uart_ui.nputs(ARRANDN("File current data sector: "));
-    System::UART::uart_ui.putu32h(fs.dsect);
-    System::UART::uart_ui.nputs(ARRANDN("\n\n\r"));
-
     vTaskDelay(100);
 
     System::UART::uart_ui.nputs(ARRANDN("opening file.txt.... \n\r res: "));
-    System::UART::uart_ui.putu32h(pf_open(" "));
+    System::UART::uart_ui.putu32h(pf_open("FILE.TXT"));
     System::UART::uart_ui.nputs(ARRANDN("\n\n\r"));
+
+    printFS(fs);
 
     vTaskDelay(100);
 
-    strcat(Line,"Do I need to panik? Probably.\r\n");
+    Line[0] = 'p';
+    Line[1] = 'a';
+    Line[2] = 'n';
+    Line[3] = 'i';
+    Line[4] = 'k';
+    Line[5] = '\0';
 
-    System::UART::uart_ui.nputs(ARRANDN("Writing.... \n\r res: "));
-    System::UART::uart_ui.putu32h(pf_write(Line, strlen(Line), &bw));
+    System::UART::uart_ui.nputs(ARRANDN("Writing.... \""));
+    System::UART::uart_ui.nputs(ARRANDN(Line));
+    System::UART::uart_ui.nputs(ARRANDN("\"\n\r res: "));
+
+    System::UART::uart_ui.putu32h(pf_write(Line, 6, &bw));
+    System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+    System::UART::uart_ui.nputs(ARRANDN("bytes written: "));
+    System::UART::uart_ui.putu32h(bw);
     System::UART::uart_ui.nputs(ARRANDN("\n\n\r"));
-
-    vTaskDelay(100);
 
     System::UART::uart_ui.nputs(ARRANDN("Finalizing.... \n\r res: "));
     System::UART::uart_ui.putu32h(pf_write(0, 0, &bw));
     System::UART::uart_ui.nputs(ARRANDN("\n\n\r"));
-
-    vTaskDelay(100);
 
     while(1);
 
 
     System::FailHard("SDC_test_task ended" NEWLINE);
     vTaskDelete(NULL);
+}
+
+void printFS(FATFS fs)
+{
+    System::UART::uart_ui.nputs(ARRANDN("FAT sub type: "));
+        System::UART::uart_ui.putu32h(fs.fs_type);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("File status: "));
+        System::UART::uart_ui.putu32h(fs.flag);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("Number of sectors per cluster: "));
+        System::UART::uart_ui.putu32h(fs.csize);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("pad1: "));
+        System::UART::uart_ui.putu32h(fs.pad1);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("Number of root directory entries (0 on FAT32): "));
+        System::UART::uart_ui.putu32h(fs.n_rootdir);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("Number of FAT entries (= number of clusters + 2): "));
+        System::UART::uart_ui.putu32h(fs.n_fatent);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("FAT start sector: "));
+        System::UART::uart_ui.putu32h(fs.fatbase);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("Root directory start sector (Cluster# on FAT32): "));
+        System::UART::uart_ui.putu32h(fs.dirbase);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("Data start sector: "));
+        System::UART::uart_ui.putu32h(fs.database);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("File R/W pointer: "));
+        System::UART::uart_ui.putu32h(fs.fptr);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("File size: "));
+        System::UART::uart_ui.putu32h(fs.fsize);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("File start cluster: "));
+        System::UART::uart_ui.putu32h(fs.org_clust);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("File current cluster: "));
+        System::UART::uart_ui.putu32h(fs.curr_clust);
+        System::UART::uart_ui.nputs(ARRANDN("\n\r"));
+
+        System::UART::uart_ui.nputs(ARRANDN("File current data sector: "));
+        System::UART::uart_ui.putu32h(fs.dsect);
+        System::UART::uart_ui.nputs(ARRANDN("\n\n\r"));
 }
 
 
